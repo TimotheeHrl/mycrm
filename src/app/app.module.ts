91760780c5c5b223f2af1867d8e358767abe20ff
timotheehrl <tharleaux@gmail.com>
2022-08-24 10:34:06 +0200

    pages orders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { CoreModule } from "./core/core.module";
+import { PageNotFoundComponentComponent } from './page-not-found-component/page-not-found-component.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PageNotFoundComponentComponent],
   imports: [BrowserModule, AppRoutingModule, CoreModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -1,14 +1,18 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
 
-import { OrdersRoutingModule } from './orders-routing.module';
-
+import { OrdersRoutingModule } from "./orders-routing.module";
+import { PageAddOrderComponentComponent } from "./pages/page-add-order-component/page-add-order-component.component";
+import { PageEditOrderComponentComponent } from "./pages/page-edit-order-component/page-edit-order-component.component";
+import { PageListOrdersComponentComponent } from "./pages/page-list-orders-component/page-list-orders-component.component";
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    OrdersRoutingModule
-  ]
+  declarations: [
+    PageAddOrderComponentComponent,
+    PageAddOrderComponentComponent,
+    PageEditOrderComponentComponent,
+    PageListOrdersComponentComponent,
+  ],
+  imports: [CommonModule, OrdersRoutingModule],
 })
-export class OrdersModule { }
+export class OrdersModule {}
