8b31728e493d0040910b27af134e09ed8dd61c39
timotheehrl <tharleaux@gmail.com>
2022-08-31 10:27:07 +0200

    efrrfe

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,60 +1,41 @@
-import { NgModule } from '@angular/core';
-<<<<<<< HEAD
-import { RouterModule, Routes } from '@angular/router';
-import { PageSignInComponent } from './login/pages/page-sign-in/page-sign-in.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { PageSignInComponent } from "./login/pages/page-sign-in/page-sign-in.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'sign-in', pathMatch: 'full' },
-  { path: 'sign-in', component: PageSignInComponent },
-=======
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+  { path: "", redirectTo: "sign-in", pathMatch: "full" },
+  { path: "sign-in", component: PageSignInComponent },
 
-const routes: Routes = [
-  { path: '', redirectTo: 'sign-in', pathMatch: 'full' },
->>>>>>> 115419498f4bc634c0c3b36fe07a8810910ac144
   {
-    path: 'orders',
+    path: "orders",
     loadChildren: () =>
-      import('./orders/orders.module').then((m) => m.OrdersModule),
-<<<<<<< HEAD
+      import("./orders/orders.module").then((m) => m.OrdersModule),
   }, // lazy loading
-=======
-  },
->>>>>>> 115419498f4bc634c0c3b36fe07a8810910ac144
+
   {
-    path: 'clients',
+    path: "clients",
     loadChildren: () =>
-      import('./clients/clients.module').then((m) => m.ClientsModule),
+      import("./clients/clients.module").then((m) => m.ClientsModule),
   },
   {
-<<<<<<< HEAD
-    path: 'login',
+    path: "login",
     loadChildren: () =>
-      import('./login/login-routing.module').then((m) => m.LoginRoutingModule),
+      import("./login/login-routing.module").then((m) => m.LoginRoutingModule),
   },
   {
-    path: 'path-not-found',
-=======
-    path: '**',
->>>>>>> 115419498f4bc634c0c3b36fe07a8810910ac144
+    path: "path-not-found",
+
     loadChildren: () =>
-      import('./page-not-found/page-not-found.module').then(
+      import("./page-not-found/page-not-found.module").then(
         (m) => m.PageNotFoundModule
       ),
   },
 ];
-<<<<<<< HEAD
 //snipets : lazy   aroute
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-=======
 
-@NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
-  ],
->>>>>>> 115419498f4bc634c0c3b36fe07a8810910ac144
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
